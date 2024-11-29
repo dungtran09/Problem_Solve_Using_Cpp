ceaa3c39a28360571e286a84ff8b744bad8aaa21
Dũng Trần <ngaynangnhe@gmail.com>
2024-11-29 19:05:41 +0700

    Update: Two Sum

--- Arrays/Two_Sum.cpp
+++ Arrays/Two_Sum.cpp
@@ -1,31 +1,26 @@
 
 #include <iostream>
-int *Two_Sum(int n[], int target, int size) {
-  int result[2];
+void Two_Sum(int n[], int target, int size) {
   for (int i = 0; i <= size; i++) {
     for (int j = 0; j <= size; j++) {
       if (n[i] == n[j])
         continue;
       if (n[i] + n[j] == target) {
-        result[0] = i;
-        result[1] = j;
-        break;
+        std::cout << "[Index = " << i << "]" << std::endl;
+        std::cout << "[Index = " << j << "]" << std::endl;
+        return;
       }
     }
   }
-
-  return result;
 }
 
 int main() {
   int nums[] = {1, 3, 4, 5, 6};
 
   int size = sizeof(nums) / sizeof(nums[0]);
-  int target = 9;
-  int *result = Two_Sum(nums, target, size);
+  int target = 11;
 
-  for (int i = 0; i < 2; i++) {
-    std::cout << result[i] << std::endl;
-  }
+  Two_Sum(nums, target, size);
+
   return 0;
 }
